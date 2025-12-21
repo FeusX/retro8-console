b32ece5d822f8cd9921552faaef8f079065b6bee
feusx <feusuaiga@gmail.com>
2025-12-21 12:09:07 +0300

    bigger alien invader

--- main/main.c
+++ main/main.c
@@ -5,27 +5,22 @@
 
 void app_main(void)
 {
-  printf("Initializing SSD1306...\n");
-  ssd1306_init();
-
-  int16_t inv_x = 60;
-  int16_t inv_y = 0;
-  int16_t y_speed = 1;
-  static const uint8_t invader_sprite[] = {0x18, 0x3C, 0x7E, 0xDB, 0xFF, 0x24, 0x5A, 0xA5};
-
-  while(1)
-  {
+    printf("Initializing SSD1306...\n");
+    ssd1306_init();
+    
+    static const uint8_t invader_lt[] = {0x80, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC, 0xFE, 0xFF};
+    static const uint8_t invader_lb[] = {0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF};
+    static const uint8_t invader_rt[] = {0xE7, 0xE7, 0xFF, 0x18, 0xE6, 0x18, 0x66, 0x00};
+    static const uint8_t invader_rb[] = {0xE7, 0xE7, 0xFF, 0x18, 0x67, 0x18, 0x66, 0x00};
     ssd1306_clear();
-    inv_y += y_speed;
-
-    if(inv_y >= 56 || inv_y <= 0)
-    {
-      y_speed *= -1;
-    }
-
-    ssd1306_draw_sprite(inv_x, inv_y, 8, 8, invader_sprite);
+    
+    ssd1306_draw_sprite(50, 20, 8, 8, invader_lt);
+    ssd1306_draw_sprite(58, 20, 8, 8, invader_rt);
+    ssd1306_draw_sprite(50, 28, 8, 8, invader_lb);
+    ssd1306_draw_sprite(58, 28, 8, 8, invader_rb);
     ssd1306_update();
 
-    vTaskDelay(pdMS_TO_TICKS(16));
-  }
+    while(1) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
