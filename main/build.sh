5a7127a3503aa9942bc83dcf30049fe4319fd836
FeusX <132133667+FeusX@users.noreply.github.com>
2025-12-30 14:18:11 +0300

    Update build.sh

--- main/build.sh
+++ main/build.sh
@@ -3,7 +3,7 @@
 source $HOME/esp/esp-idf/export.sh > /dev/null 2>&1
 
 sudo chmod a+rw /dev/ttyUSB0
-cd ~/Documents/esp32-console
+cd ~/Documents/retro8-console
 idf.py build
 idf.py flash monitor
 cd main
