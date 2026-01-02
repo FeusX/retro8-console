95c5f1c3411fdbf6937b6885d55218d5c8664744
feusuaiga@gmail.com <feusuaiga@gmail.com>
2026-01-02 21:55:45 +0300

    prototyping tetris assets

--- main/components/assets.h
+++ main/components/assets.h
@@ -13,4 +13,18 @@ static const uint8_t player_bunker_sprite[] = {
   0x5A, 0xDB, 0xDB, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF  
 };
 
+// ---- TETRIS ----
+const uint16_t tetrominoes[7][4] = {
+  { // I
+    0x0F00,
+    0x2222,
+    0x0F00,
+    0x4444
+  },
+
+  { // T
+    // brb
+  },
+};
+
 #endif
