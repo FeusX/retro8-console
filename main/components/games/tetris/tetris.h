5683370b7d97512ee5bb1bf2cbe43f9c3ff1851d
feusx <feusuaiga@gmail.com>
2026-01-04 19:09:33 +0300

    hmm

--- main/components/games/tetris/tetris.h
+++ main/components/games/tetris/tetris.h
@@ -1,30 +1,58 @@
 #ifndef TETRIS_H
 #define TETRIS_H
 
-#define FIELD_W 10
-#define FIELD_H 20
+#define BOARD_WIDTH  10
+#define BOARD_HEIGHT 20
+#define BLOCK_SIZE   3
 
+#include "esp_system.h"
+#include "esp_random.h"
+
+#include "../../ssd1306.h"
 #include "../../assets.h"
 
-uint8_t field[FIELD_H][FIELD_W];
+struct {
+  int x, y;
+  int type;
+  int rotation;
+} current_piece;
 
-static void draw_block(int gx, int gy)
+uint8_t board[BOARD_WIDTH][BOARD_HEIGHT] = {0};
+
+static inline bool check_collision(int next_x, int next_y, int next_rot)
 {
-  int px = gx * 4;
-  int py = gy * 4;
+  uint16_t mask = pieces[current_piece.type].rotation[next_rot];
 
-  for(int y = 0; y < 4; y++)
+  for(int i = 0; i < 16; i++)
   {
-    for(int x = 0; x < 4; x++)
+    if(mask & 0x8000 >> i)
     {
-      ssd1306_draw_pixel(px + x, py + y, true);
+      int px = next_x + (i % 4);
+      int py = next_y + (i / 4);
+      if(px < 0 || px >= BOARD_WIDTH || py > BOARD_HEIGHT) return true;
+      if(py >= 0 && board[px][py]) return true;
     }
+  }
+
+  return false;
+}
+
+static inline void spawn piece()
+{
+  current_piece.type = esp_random() % 7;
+  current_piece.rotation = 0;
+  current_piece.x = BOARD_WIDTH / 2 - 2;
+  current_piece.y = 0;
+
+  if(check_collision(current_piece.x, current_piece.y, current_piece.rotation))
+  {
+    for(int x=0; x<BOARD_WIDTH; x++) for(int y=0; y<BOARD_HEIGHT; y++) board[x][y] = 0;
   }
 }
 
 static void run_tetris(void)
 {
-  draw_block(20, 30);
+  
 }
 
 #endif
