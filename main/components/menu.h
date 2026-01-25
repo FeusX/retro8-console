d9f832b43ba3ccfe2b88bc59679bb3fd0c02ece9
FeusX <132133667+FeusX@users.noreply.github.com>
2026-01-25 22:34:58 +0300

    Update menu.h

--- main/components/menu.h
+++ main/components/menu.h
@@ -26,55 +26,60 @@ static bool last_up = false;
 
 static inline void run_menu(void)
 {
-  bool current_down = is_pressed(BTN_DOWN);
-  bool current_up = is_pressed(BTN_UP);
+  while(1)
+  {
+    bool current_down = is_pressed(BTN_DOWN);
+    bool current_up = is_pressed(BTN_UP);
   
-  ssd1306_clear();
-  draw_string(15, 0, "---- RETRO8 ----");
+    ssd1306_clear();
+    draw_string(15, 0, "---- RETRO8 ----");
 
-  for(int i = 0; i < 3; i++)
-  {
-    int8_t idx = i + scroll_offset;
-
-    if(idx < GAME_COUNT)
+    for(int i = 0; i < 3; i++)
     {
-      int16_t y = 20 + (i * 12);
-      if(menu_cursor == idx)
-      { draw_string(10, y, ">"); }
-      draw_string(25, y, games[idx].name);
-    }
-  }
+      int8_t idx = i + scroll_offset;
 
-  if(current_down && !last_down)
-  {
-    menu_cursor++;
-    if(menu_cursor >= GAME_COUNT)
+      if(idx < GAME_COUNT)
+      {
+        int16_t y = 20 + (i * 12);
+        if(menu_cursor == idx)
+        { draw_string(10, y, ">"); }
+        draw_string(25, y, games[idx].name);
+      }
+    }
+
+    if(current_down && !last_down)
     {
-      menu_cursor = 0;
-      scroll_offset = 0;
+      menu_cursor++;
+      if(menu_cursor >= GAME_COUNT)
+      {
+        menu_cursor = 0;
+        scroll_offset = 0;
+      }
+      else if(menu_cursor >= scroll_offset + 3)
+      { scroll_offset++; }
     }
-    else if(menu_cursor >= scroll_offset + 3)
-    { scroll_offset++; }
-  }
-  last_down = current_down;
+    last_down = current_down;
 
-  if(current_up && !last_up)
-  {
-    menu_cursor--;
-    if(menu_cursor < 0)
+    if(current_up && !last_up)
     {
-      menu_cursor = GAME_COUNT - 1;
-      scroll_offset = (GAME_COUNT > 3) ? GAME_COUNT - 3 : 0;
+      menu_cursor--;
+      if(menu_cursor < 0)
+      {
+        menu_cursor = GAME_COUNT - 1;
+        scroll_offset = (GAME_COUNT > 3) ? GAME_COUNT - 3 : 0;
+      }
+      else if(menu_cursor < scroll_offset)
+      { scroll_offset--; }
     }
-    else if(menu_cursor < scroll_offset)
-    { scroll_offset--; }
+    last_up = current_up;
+
+    if(is_pressed(BTN_A))
+    { current_state = games[menu_cursor].state; }
+
+    ssd1306_update();
+
+    vTaskDelay(pdMS_TO_TICKS(33));
   }
-  last_up = current_up;
-
-  if(is_pressed(BTN_A))
-  { current_state = games[menu_cursor].state; }
-
-  ssd1306_update();
 }
 
 #endif
