28b4bf348afdce69e1df61be81909452723a5922
feusx <feusuaiga@gmail.com>
2026-02-19 21:56:44 +0300

    almost finished asteroid

--- main/components/games/asteroid/asteroid.h
+++ main/components/games/asteroid/asteroid.h
@@ -1,14 +1,37 @@
 #ifndef ASTEROID_H
 #define ASTEROID_H
 
+#include "../../ssd1306.h"
+#include "../../font.h"
+
 #include "renderer.h"
 #include "player.h"
 #include "balls.h"
 
-/*static inline void ast_collision_detect()
+static inline void ast_collision_detect()
 {
-  
-}*/
+  for(int i = 0; i < MAX_ASTEROIDS; i++)
+  {
+    if(asteroids[i].is_destroyed) continue;
+
+    int16_t dx = abs(player.x - (asteroids[i].x >> 4));
+
+    if(dx > asteroids[i].rad) continue;
+    
+    int16_t dy = abs(player.y - (asteroids[i].y >> 4));
+
+    if(dy > asteroids[i].rad) continue; 
+
+    if((dx * dx) + (dy * dy) < asteroids[i].rad * asteroids[i].rad)
+    {
+      ssd1306_clear();
+      draw_string(30, 24, "TEST");
+      ssd1306_update();
+
+      vTaskDelay(pdMS_TO_TICKS(50));
+    }
+  }
+}
 
 static inline void run_asteroid(void)
 {
@@ -24,7 +47,7 @@ static inline void run_asteroid(void)
     update_asteroids();
     ast_update_player();
 
-    //ast_collision_detect();
+    ast_collision_detect();
 
     render_asteroids();
     
